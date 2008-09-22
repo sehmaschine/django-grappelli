afacfc0c9574ab30b7810a9552010ff0ce4e03a9
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2008-09-22 09:38:51 +0000

    add youtube-plugin

---
+++ media/tinymce_2/jscripts/tiny_mce/plugins/youtube/langs/en.js
@@ -0,0 +1,3 @@
+tinyMCE.addI18n('en.youtube',{
+	desc : 'Insert YouTube video'
+});
---
+++ media/tinymce_2/jscripts/tiny_mce/plugins/youtube/langs/en_dlg.js
@@ -0,0 +1,7 @@
+tinyMCE.addI18n('en.youtube_dlg',{
+    title : "YouTube Video",
+    instr : "Insert the YouTube video ID of the video you want to appear.",
+    ytID : "ID",
+    ytW : "Width",
+    ytH : "Height"
+});
