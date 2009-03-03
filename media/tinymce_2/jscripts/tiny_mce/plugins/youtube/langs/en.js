444fb588bc7606b7d3614f96724c1ee52192fc5f
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2009-03-03 12:21:11 +0000

    deleted old tinymce

--- media/tinymce_2/jscripts/tiny_mce/plugins/youtube/langs/en.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('en.youtube',{
-	desc : 'Insert YouTube video'
-});
--- media/tinymce_2/jscripts/tiny_mce/plugins/youtube/langs/en_dlg.js
+++
@@ -1,7 +0,0 @@
-tinyMCE.addI18n('en.youtube_dlg',{
-    title : "YouTube Video",
-    instr : "Insert the YouTube video ID of the video you want to appear.",
-    ytID : "ID",
-    ytW : "Width",
-    ytH : "Height"
-});
