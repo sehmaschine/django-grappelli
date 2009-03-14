4d2d52af56611514b78f6be78104adc673d45dab
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2009-03-14 15:38:37 +0000

    admin for navigation

---
+++ admin.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from django.utils.translation import ugettext as _
+
+from grappelli.models.navigation import Navigation, NavigationItem
+
+
+class NavigationItemInline(admin.StackedInline):
+    model = NavigationItem
+    extra = 1
+    sortable = True
+    fieldsets = (
+        ('', {
+            'fields': ('title', 'link', 'category',)
+        }),
+        (_('Groups/Users'), {
+            'classes': 'collapse-open',
+            'fields': ('groups', 'users',),
+        }),
+    )
+    
+
+class NavigationOptions(admin.ModelAdmin):
+    save_as = True
+    list_display = ('order', 'title',)
+    list_display_links = ('title',)
+    fieldsets = (
+        ('', {
+            'fields': ('title', 'order',)
+        }),
+    )
+    inlines = [NavigationItemInline]
+    
+
+admin.site.register(Navigation, NavigationOptions)
+
