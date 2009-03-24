66fb0086ada97173eddf26f1ea9402527a670be9
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2009-03-24 11:19:19 +0000

    add related-view

---
+++ views/related.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+from django.db import models
+
+
+def related_lookup(request):
+    
+    if request.method == 'GET':
+        if request.GET.has_key('object_id') and request.GET.has_key('app_label') and request.GET.has_key('model_name'):
+            object_id = request.GET.get('object_id')
+            app_label = request.GET.get('app_label')
+            model_name = request.GET.get('model_name')
+            try:
+                model = models.get_model(app_label, model_name)
+                obj = model.objects.get(pk=object_id)
+                obj = unicode(obj)
+            except:
+                obj = "Not Found"
+        else:
+            obj = "Not Found"
+    else:
+        obj = "Not Found"
+    
+    return HttpResponse(obj, mimetype='text/plain; charset=utf-8')
+    
+
