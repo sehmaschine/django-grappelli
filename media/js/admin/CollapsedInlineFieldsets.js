7899229e6674176d704e5fa4878966d470ad9c22
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2009-08-11 08:10:18 +0000

    g2: added admin-js again

---
+++ media/js/admin/CollapsedInlineFieldsets.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    
+    /// INLINE ELEMENTS
+    /// collapsible elements for stacked inlines
+    $('div.inline-stacked div.inline-related').each(function(i) {
+        $(this).addClass("collapsed");
+        $(this).find('h3:first').attr("class", "collapse-toggle");
+    });
+    $('div.inline-stacked div.inline-related h3.collapse-toggle').bind("click", function(){
+        $(this).parent().toggleClass('collapsed');
+        $(this).parent().toggleClass('collapse-closed');
+        $(this).parent().toggleClass('collapse-open');
+    });
+    
+    /// INLINEGROUPS (STACKED & TABULAR)
+    $('div.inline-group.collapse-closed').each(function() {
+        $(this).addClass("collapsed");
+        $(this).find('h2:first').attr("class", "collapse-toggle");
+    });
+    $('div.inline-group.collapse-open').each(function() {
+        $(this).find('h2:first').attr("class", "collapse-toggle");
+    });
+    $('div.inline-group h2.collapse-toggle').bind("click", function(){
+        $(this).parent().toggleClass('collapsed');
+        $(this).parent().toggleClass('collapse-closed');
+        $(this).parent().toggleClass('collapse-open');
+    });
+    
+    /// OPEN STACKEDINLINE WITH ERRORS (onload)
+    $('div.inline-stacked div.inline-related').find('div[class*="errors"]:first').each(function(){
+        $(this).parents('div.inline-related').removeClass("collapsed");
+        $(this).parents('div.inline-stacked').removeClass("collapsed");
+    });
+    
+    /// OPEN TABULARINLINE WITH ERRORS (onload)
+    $('div.inline-tabular').find('div[class*="error"]:first').each(function(i) {
+        $(this).parents('div.inline-tabular').removeClass("collapsed");
+    });
+    
+});
