ec98819326d91e34ff51b0b39d4191f54b1461c1
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2009-08-13 11:14:50 +0000

    updated help-view, included translation

--- views/help.py
+++ views/help.py
@@ -4,10 +4,14 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.translation import ugettext as _
 
 from grappelli.models.help import Help, HelpItem
 
 def detail(request, object_id):
+    """
+    CMS Help (Detail).
+    """
     
     obj = get_object_or_404(HelpItem, pk=object_id)
     menu = Help.objects.filter(helpitem__isnull=False).distinct()
@@ -18,14 +22,16 @@ def detail(request, object_id):
         'title': obj.title,
     }, context_instance=RequestContext(request) )
 detail = staff_member_required(detail)
-    
 
 def help(request):
+    """
+    CMS Help (Overview).
+    """
     
     menu = Help.objects.filter(helpitem__isnull=False).distinct()
     
     return render_to_response('grappelli/help/help.html', {
         'menu': menu,
-        'title': 'Help',
+        'title': _('Help'),
     }, context_instance=RequestContext(request) )
 help = staff_member_required(help)
