f152878a25245762a8ec8be100fa23f2e867144f
Maxime Haineault <haineault@gmail.com>
2009-08-20 11:42:20 +0000

    - changed autofocus method for something less error prone - added french translation

--- media/jquery/jquery.grappelli.js
+++ media/jquery/jquery.grappelli.js
@@ -4,6 +4,7 @@
     $(function(){
         // always focus first field of the form
         $('form .form-row:eq(0)').find('input, select, textarea, button').eq(0).focus();
+        $('#searchbar').focus();
     });
 
 })(jQuery);
