98ba706abf9e640d01791f3bb0c4a5e87b66d30e
Maxime Haineault <haineault@gmail.com>
2009-08-20 23:53:20 +0000

    removed Bookmark.js

--- media/jquery/jquery.grappelli.js
+++ media/jquery/jquery.grappelli.js
@@ -16,6 +16,23 @@
         var bookmarksURL = $.grappelli.urls.bookmarks +'?path='+ window.location.pathname;
         $('#bookmarks').load(bookmarksURL +' #bookmarks > li');
 
+        $("li#toggle-bookmarks-listing.enabled").live("mouseover", function(){
+            $("#bookmarks-listing").show();
+        }).live("mouseout", function(){
+            $("#bookmarks-listing").hide();
+        });
+        $('#toggle-bookmark-add').live("click", function() {
+            $("input#bookmark-title").val($('h1').text());
+            $("input#bookmark-path").val(window.location.pathname);
+            $("#bookmark-add").show();
+            $("#toggle-bookmarks-listing").removeClass('enabled');
+        });
+        $('#bookmark-add-cancel').live("click", function() {
+            $("#bookmark-add").hide();
+            $("#toggle-bookmarks-listing").toggleClass('enabled');
+            return false;
+        });
+
     });
 
 })(jQuery);
