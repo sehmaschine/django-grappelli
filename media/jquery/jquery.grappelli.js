edbdd2ae7436bbd901ddf8b87ba32146172b345b
Maxime Haineault <haineault@gmail.com>
2009-08-20 23:50:02 +0000

    - simplified bookmark loading - added/fixed some french translations

--- media/jquery/jquery.grappelli.js
+++ media/jquery/jquery.grappelli.js
@@ -1,10 +1,21 @@
 (function($){
+    
+    $.grappelli = {
+        urls: {}
+    };
 
     // INIT
     $(function(){
-        // always focus first field of the form
-        $('form .form-row:eq(0)').find('input, select, textarea, button').eq(0).focus();
-        $('#searchbar').focus();
+
+        // Always focus first field of a form OR the search input
+        $('form .form-row:eq(0)')
+            .find('input, select, textarea, button').eq(0)
+            .add('#searchbar').focus();
+
+        // Load bookmarks
+        var bookmarksURL = $.grappelli.urls.bookmarks +'?path='+ window.location.pathname;
+        $('#bookmarks').load(bookmarksURL +' #bookmarks > li');
+
     });
 
 })(jQuery);
