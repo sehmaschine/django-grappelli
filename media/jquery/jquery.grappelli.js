a1b423a7c3ea8d494d901719559048378f842837
Maxime Haineault <haineault@gmail.com>
2009-08-21 20:51:09 +0000

    - created a jQuery UI theme (look in admin/base.html for a link to it) - created a jQuery Ui widget to control bookmarks with some effects (turned off by default) - updated jQuery UI 1.7.1 to 1.7.2

--- media/jquery/jquery.grappelli.js
+++ media/jquery/jquery.grappelli.js
@@ -1,43 +1,61 @@
-(function($){
+$(function(){
+    // Always focus first field of a form OR the search input
+    $('form .form-row:eq(0)')
+        .find('input, select, textarea, button').eq(0)
+        .add('#searchbar').focus();
+});
+
+// BOOKMARKS
+
+$.widget('ui.gBookmarks', {
+         
+    _init: function() {
+        var ui  = this;
+        var url = ui.options.url +'?path='+ window.location.pathname +' #bookmarks > li';
+        
+        this.showMethod = this.options.effects && 'slideDown' || 'show';
+        this.hideMethod = this.options.effects && 'slideUp' || 'hide';
+
+        $("li#toggle-bookmarks-listing.enabled")
+            .live("mouseover", function(){ ui.show("#bookmarks-listing:hidden"); });
+        
+        $('#toggle-bookmark-add').live("click", function() { return ui.add(); });
+        $('#bookmark-add-cancel').live("click", function() { return ui.cancel(); });
+        ui.element.load(url);
+    },
+
+    show: function(el) {
+        var ui = this;
+        $(el)[ui.showMethod]();
+        $("#bookmarks").one("mouseleave", function(){ 
+            ui.hide("#bookmarks-listing:visible"); 
+        });
+    },
+
+    hide: function(el) {
+        $(el)[this.hideMethod]();
+    },
+
+    cancel: function() {
+        this.hide("#bookmark-add");
+        $("#toggle-bookmarks-listing").toggleClass('enabled');
+        return false;
+    },
     
-    $.grappelli = {
-        urls: {}
-    };
+    add: function() {
+        $("#bookmark-title").val($('h1').text());
+        $("#bookmark-path").val(window.location.pathname);
+        $("#toggle-bookmarks-listing").removeClass('enabled');
+        this.show("#bookmark-add");
+        return false;
+    }
+});
 
-    // INIT
-    $(function(){
+$.ui.gBookmarks.defaults = {
+    url: '/grappelli/bookmark/get/',
+    effects: false
+};
 
-        // Always focus first field of a form OR the search input
-        $('form .form-row:eq(0)')
-            .find('input, select, textarea, button').eq(0)
-            .add('#searchbar').focus();
+// 
 
-        // Bookmarks: init
-        var bookmarksURL = $.grappelli.urls.bookmarks +'?path='+ window.location.pathname;
-        $('#bookmarks').load(bookmarksURL +' #bookmarks > li');
 
-        // Bookmarks: mouse interactions 
-        $("li#toggle-bookmarks-listing.enabled").live("mouseover", function(){
-            $("#bookmarks-listing").show();
-        }).live("mouseout", function(){
-            $("#bookmarks-listing").hide();
-        });
-        
-        // Bookmarks: add
-        $('#toggle-bookmark-add').live("click", function() {
-            $("input#bookmark-title").val($('h1').text());
-            $("input#bookmark-path").val(window.location.pathname);
-            $("#bookmark-add").show();
-            $("#toggle-bookmarks-listing").removeClass('enabled');
-        });
-        
-        // Bookmarks: cancel
-        $('#bookmark-add-cancel').live("click", function() {
-            $("#bookmark-add").hide();
-            $("#toggle-bookmarks-listing").toggleClass('enabled');
-            return false;
-        });
-
-    });
-
-})(jQuery);
