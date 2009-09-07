fc3f85a2ed2dbfeb1f7b4f0ac7fb42804042d941
Maxime Haineault <haineault@gmail.com>
2009-09-07 17:26:58 +0000

    jqueryfied urlify.js, it doesn't seem to be used anywhere so I moved it in the jquery folder. If we manage to distinguish slug fields I'll be able to do something with it ..

--- media/js/core.js
+++ media/js/core.js
@@ -2,7 +2,7 @@
 
 // basic browser identification & version
 var isOpera = (navigator.userAgent.indexOf("Opera")>=0) && parseFloat(navigator.appVersion);
-var isIE = ((document.all) && (!isOpera)) && parseFloat(navigator.appVersion.split("MSIE ")[1].split(";")[0]);
+var isIE = $.browser.msie;
 
 // Cross-browser event handlers.
 function addEvent(obj, evType, fn) {
