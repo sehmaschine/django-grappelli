fb07f626bed41600fb2b3f8098124977ad0848dc
Maxime Haineault <haineault@gmail.com>
2009-09-07 17:01:43 +0000

    - jQueryfied actions.js

--- media/jquery/jquery.grappelli.js
+++ media/jquery/jquery.grappelli.js
@@ -9,7 +9,17 @@ $(function(){
     });
 });
 
+// ACTIONS
 
+$.widget('ui.gActions', {
+    _init: function() {
+        var ui = this;
+        $('#action-toggle').show().bind('click.grappelli', function(){
+            ui.element.find('.result-list input.action-select').attr('checked', $(this).attr('checked'));
+        
+        });
+    }
+});
 
 
 // BOOKMARKS
