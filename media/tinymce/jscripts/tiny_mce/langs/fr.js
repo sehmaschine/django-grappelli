d072212dae02857fbf772188bf1d44e0f4749063
Maxime Haineault <haineault@gmail.com>
2009-09-29 12:31:40 +0000

    - added French translation for TinyMCE

---
+++ media/tinymce/jscripts/tiny_mce/langs/fr.js
@@ -0,0 +1,154 @@
+tinyMCE.addI18n({en:{
+common:{
+edit_confirm:"Voulez-vous utiliser le mode WYSIWYG pour cette zone de texte?",
+apply:"Appliquer",
+insert:"Insérer",
+update:"Mettre à jour",
+cancel:"Annuler",
+close:"Fermer",
+browse:"Parcourir",
+class_name:"Classe",
+not_set:"-- Non établies --",
+clipboard_msg:"Copier / Couper / Coller n'est pas disponible sous Mozilla et Firefox. Désirez-vous plus d'informations sur cette problème?",
+clipboard_no_support:"Actuellement non supporté par votre navigateur, utilisez les raccourcis clavier à la place.",
+popup_blocked:"Désolé, mais nous avons remarqué que votre popup-blocker a désactivé une fenêtre qui fournit des fonctionnalités à cette application. Il est nécéssaire de désactiver le blocage sur ce site afin d'utiliser pleinement cet outil.",
+invalid_data:"Erreur: Valeurs incorrectes entrées, les erreurs sont marquées en rouge.",
+more_colors:"Plus de couleurs"
+},
+contextmenu:{
+align:"Alignment",
+left:"Gauche",
+center:"Centre",
+right:"Droit",
+full:"Remplir"
+},
+insertdatetime:{
+date_fmt:"%Y-%m-%d",
+time_fmt:"%H:%M:%S",
+insertdate_desc:"Insérer date",
+inserttime_desc:"Insérer heure",
+months_long:"Janvier,Février,Mars,Avril,Mai,Juin,Juillet,Août,Septembre,Octobre,Novembre,Décembre",
+months_short:"Jan,Fév,Mar,Avr,Mai,Jun,Jui,Août,Sep,Oct,Nov,Déc",
+day_long:"Dimanche,Lundi,Mardi,Mercredi,Jeudi,Vendredi,Samedi,Dimanche",
+day_short:"Dim,Lun,Mar,Mer,Jeu,Ven,Sam,Dim"
+},
+print:{
+print_desc:"Imprimer"
+},
+preview:{
+preview_desc:"Prévisualiser"
+},
+directionality:{
+ltr_desc:"Direction de gauche à droite",
+rtl_desc:"Direction de droite à gauche"
+},
+layer:{
+insertlayer_desc:"Insérer un nouveau calque",
+forward_desc:"Avancer",
+backward_desc:"Reculer",
+absolute_desc:"Activer/désactiver le positionnement absolu",
+content:"Nouveau calque ..."
+},
+save:{
+save_desc:"Sauver",
+cancel_desc:"Annuler tous les changements"
+},
+nonbreaking:{
+nonbreaking_desc:"Insérer un espace insécable"
+},
+iespell:{
+iespell_desc:"Exécuter la vérification orthographique",
+download:"ieSpell non détecté. Voulez-vous l'installer maintenant?"
+},
+advhr:{
+advhr_desc:"Règle horizontale"
+},
+emotions:{
+emotions_desc:"Émotions"
+},
+searchreplace:{
+search_desc:"Chercher",
+replace_desc:"Chercher/Remplacer"
+},
+advimage:{
+image_desc:"Insérer/modifier image"
+},
+advlink:{
+link_desc:"Insérer/modifier liens"
+},
+xhtmlxtras:{
+cite_desc:"Citation",
+abbr_desc:"Abréviation",
+acronym_desc:"Acronyme",
+del_desc:"Suppression",
+ins_desc:"Insertion",
+attribs_desc:"Insérer/modifier Attributs"
+},
+style:{
+desc:"Modifier Style CSS"
+},
+paste:{
+paste_text_desc:"Coller en tant que texte simple",
+paste_word_desc:"Coller en provenance de Word",
+selectall_desc:"Sélectionnez Tous"
+},
+paste_dlg:{
+text_title:"Utiliser la touche CTRL + V sur votre clavier pour coller le texte dans la fenêtre.",
+text_linebreaks:"Garder les sauts de ligne",
+word_title:"Utiliser la touche CTRL + V sur votre clavier pour coller le texte dans la fenêtre."
+},
+table:{
+desc:"Insérer un tableau",
+row_before_desc:"Insérer une rangé avant",
+row_after_desc:"Insérer une rangé après",
+delete_row_desc:"Supprimer la rangé",
+col_before_desc:"Insérer une colonne avant",
+col_after_desc:"Insérer une colonne après",
+delete_col_desc:"Supprimer la colonne",
+split_cells_desc:"Défusionner les cellules",
+merge_cells_desc:"Fusionner les cellules",
+row_desc:"Propriétés de la rangé",
+cell_desc:"Propriétés de la cellule",
+props_desc:"Propriétés du tableau",
+paste_row_before_desc:"Coller la rangé avant",
+paste_row_after_desc:"Coller la rangé après",
+cut_row_desc:"Couper la rangé",
+copy_row_desc:"Copiez la rangé",
+del:"Supprimer le tableau",
+row:"Rangé",
+col:"Colonne",
+cell:"Cellule"
+},
+autosave:{
+unload_msg:"The changes you made will be lost if you navigate away from this page."
+},
+fullscreen:{
+desc:"Basculer en mode plein écran"
+},
+media:{
+desc:"Insérer/modifier média",
+edit:"Modifier média"
+},
+fullpage:{
+desc:"Propriétés du Document"
+},
+template:{
+desc:"Insérer modèle prédéfini"
+},
+visualchars:{
+desc:"Caractères de contrôle visuel"
+},
+spellchecker:{
+desc:"Basculer la vérification d'orthographe",
+menu:"Préférences de la vérification d'orthographe",
+ignore_word:"Ignorer mot",
+ignore_words:"Ignorer tout",
+langs:"Langues",
+wait:"Veuillez attendre...",
+sug:"Suggestions",
+no_sug:"Pas de suggestions",
+no_mpell:"Aucune erreur trouvée."
+},
+pagebreak:{
+desc:"Insérer saut de page."
+}}});
