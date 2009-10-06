096c48728892ce9f9696749ffc3ab27dee489f1e
Maxime Haineault <haineault@gmail.com>
2009-10-06 00:31:27 +0000

    created inline tabular/stacked tests

--- testproject/testapp/admin.py
+++ testproject/testapp/admin.py
@@ -7,7 +7,7 @@ admin_site = admin.AdminSite()
 #if hasattr(admin.site, 'disable_action'):
 #    admin.site.disable_action('delete_selected')
 
-from testapp.models import GrappelliFields, DjangoFields
+from testapp.models import GrappelliFields, DjangoFields, InlineTabularTest, InlineStackedTest
 from grappelli.widgets import AutocompleteSearchInput, M2MAutocompleteSearchInput
 
 # -- User -- Overriding grappelli test
@@ -24,6 +24,32 @@ admin_site.register(User, UserAdmin)
 
 class DjangoFieldsAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'char_test', 'url_test')
+    fieldsets = (
+        (None, {
+            'fields': ('char_test', 'text_test', 'slug_test', 'boolean_test', 'nboolean_test')
+        }),
+        ('Date and Time (collapse-open)', {
+            'classes': ('collapse-open',),
+            'fields': ('datetime_test', 'time_test', 'date_test')
+        }),
+        ('Networking (collapsed)', {
+            'classes': ('collapse',),
+            'fields': ('url_test', 'email_test', 'ip_test')
+        }),
+        ('Numbers', {
+            'classes': ('collapse',),
+            'fields': ('decimal_test', 'integer_test', 'pinteger_test', 'psinteger_test', 'sinteger_test')
+        }),
+        ('Uploads', {
+            'classes': ('collapse',),
+            'fields': ('file_test', 'image_test', )
+        }),
+        ('Relationships', {
+            'classes': ('collapse',),
+            'fields': ('fk_test', 'm2m_test', 'ooo_test')
+        }),
+    )
+    
 
 admin.site.register(DjangoFields, DjangoFieldsAdmin)
 
@@ -33,6 +59,13 @@ class GrappelliFieldsAdmin(admin.ModelAdmin):
     autocomplete = {
         'fk_test': {
             'search_fields': ('username', 'first_name', 'last_name'),         # mandatory (should it?)
+            'input_format':  '{label:s}',           # optional
+            'list_format':   '{id:d} - {label:s}',  # optional
+        }
+    }
+    facelist = {
+        'm2m_test': {
+            'search_fields': ('username', 'first_name', 'last_name'),
             'input_format':  '{label:s}',           # optional
             'list_format':   '{id:d} - {label:s}',  # optional
         }
@@ -48,12 +81,78 @@ class GrappelliFieldsAdmin(admin.ModelAdmin):
         if isinstance(db_field, models.ForeignKey) and hasattr(self, 'autocomplete') and db_field.name in self.autocomplete:
             kwargs['widget'] = AutocompleteSearchInput(db_field, self)
        
-        if isinstance(db_field, models.ManyToManyField) and db_field.name in self.m2m_autocomplete_search_fields:
-            kwargs['widget'] = M2MAutocompleteSearchInput(db_field.rel, self.m2m_autocomplete_search_fields[db_field.name])
+        if isinstance(db_field, models.ManyToManyField) and hasattr(self, 'facelist') and db_field.name in self.facelist:
+            kwargs['widget'] = M2MAutocompleteSearchInput(db_field, self)
        
         return super(GrappelliFieldsAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 admin.site.register(GrappelliFields, GrappelliFieldsAdmin)
 
+
+
+
+
+
+
+class DjangoTabularFieldsInline(admin.TabularInline):
+    model = DjangoFields
+    classes = ('collapse-open',),
+    allow_add = True
+    extra = 1
+    fieldsets = (
+        (None, {
+            'fields': (
+               #'auto_test',     
+                'boolean_test', 
+               #'char_test',     
+                'date_test', 
+                'datetime_test', 
+               #'decimal_test',  
+               #'email_test',    
+               #'file_test',     
+               #'float_test',    
+               #'image_test',    
+               #'integer_test',  
+               #'ip_test',       
+                'nboolean_test', 
+               #'pinteger_test', 
+               #'psinteger_test',
+               #'slug_test',     
+               #'sinteger_test', 
+               #'text_test',     
+                'time_test', 
+               #'url_test',      
+               #'fk_test',       
+               #'inline_test',   
+                'm2m_test',
+            )
+        }),
+    )
+
+class GrappelliTabularFieldsInline(admin.TabularInline):
+    model = GrappelliFields 
+    classes = ('collapse-open',),
+    allow_add = True
+    extra = 1
+
+class DjangoStackedFieldsInline(admin.StackedInline):
+    model = DjangoFields
+    classes = ('collapse-open',),
+    allow_add = True
+
+class GrappelliStackedFieldsInline(admin.StackedInline):
+    model = GrappelliFields 
+    classes = ('collapse-open',),
+    allow_add = True
+
+class InlineStackedTestAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__',)
+    inlines = [DjangoStackedFieldsInline, GrappelliStackedFieldsInline]
+admin.site.register(InlineStackedTest, InlineStackedTestAdmin)
+
+class InlineTabularTestAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__',)
+    inlines = [DjangoTabularFieldsInline, GrappelliTabularFieldsInline]
+admin.site.register(InlineTabularTest, InlineTabularTestAdmin)
 
 #class MyAdmin(admin.ModelAdmin):
 #    # autocomplete
--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -11,27 +11,82 @@ from django.contrib.auth.models import User
 
 
 
-class DjangoFields(models.Model):
-    fk_test   = models.ForeignKey(User, verbose_name=u"Foreign Key", blank=True)
+class InlineStackedTest(models.Model):
     char_test = models.CharField(u"Char Field", max_length=255, blank=True)
-    url_test  = models.URLField(u"URL Field", max_length=255, blank=True, verify_exists=False)
-    text_test = models.TextField(u"Text Field", blank=True)
+    date_added  = models.DateField(u"DateField", auto_now_add=True, auto_now=True, blank=True, null=True)
+
+    def __unicode__(self):
+        return u'%s' % self.char_test
+
+
+class InlineTabularTest(models.Model):
+    char_test = models.CharField(u"Char Field", max_length=255, blank=True)
+    date_added  = models.DateField(u"DateField", auto_now_add=True, auto_now=True, blank=True, null=True)
+
+    def __unicode__(self):
+        return u'%s' % self.char_test
+
+
+class DjangoFields(models.Model):
+    auto_test      = models.CharField(u"AutoField", max_length=255, blank=True)
+    boolean_test   = models.BooleanField(u"BooleanField", blank=True, default=True)
+    char_test      = models.CharField(u"CharField", max_length=255, blank=True)
+    # CommaSeparatedIntegerField
+    date_test      = models.DateField(u"DateField", blank=True)
+    datetime_test  = models.DateTimeField(u"DateTimeField", blank=True)
+    decimal_test   = models.DecimalField(u"DecimalField", blank=True, max_digits=5, decimal_places=4)
+    email_test     = models.EmailField(u"EmailField", max_length=255, blank=True)
+    file_test      = models.FileField(u"FileField", blank=True, upload_to='uploads/')
+    float_test     = models.FloatField(u"FloatField", blank=True)
+    image_test     = models.ImageField(u"ImageField", blank=True, upload_to='uploads/')
+    integer_test   = models.IntegerField(u"IntegerField", blank=True)
+    ip_test        = models.IPAddressField(u"IPAddressField", blank=True)
+    nboolean_test  = models.NullBooleanField(u"NullBoolean", blank=True)
+    pinteger_test  = models.PositiveIntegerField(u"PositiveIntegerField", blank=True)
+    psinteger_test = models.PositiveSmallIntegerField(u"PositiveSmallIntegerField", blank=True)
+    slug_test      = models.SlugField(u"SlugField", max_length=50)
+    sinteger_test  = models.SmallIntegerField(u"SmallIntegerField", blank=True)
+    text_test      = models.TextField(u"TextField", blank=True)
+    time_test      = models.TimeField(u"TimeField", blank=True)
+    url_test       = models.URLField(u"URLField", max_length=255, blank=True, verify_exists=False)
+    # XMLField
+    fk_test        = models.ForeignKey(User, verbose_name=u"ForeignKey", blank=True)
+    inline_test    = models.ForeignKey('InlineTabularTest', verbose_name=u"ForeignKey (inline)", blank=True)
+    m2m_test       = models.ManyToManyField(User, verbose_name=u"ManyToManyField", blank=True, related_name='many_to_many')
+    ooo_test       = models.OneToOneField(User, verbose_name=u"OneToOneField", blank=True, related_name='one_to_one')
+
+    def __unicode__(self):
+        return u'%s' % self.char_test
 
     class Meta:
         verbose_name = u'Django Field test'
         verbose_name_plural = u'Django Field tests'
 
-    def __unicode__(self):
-        return u'%s' % self.char_test
+
+#class DjangoFields(models.Model):
+#    fk_test   = models.ForeignKey(User, verbose_name=u"Foreign Key", blank=True)
+#    char_test = models.CharField(u"Char Field", max_length=255, blank=True)
+#    url_test  = models.URLField(u"URL Field", max_length=255, blank=True, verify_exists=False)
+#    text_test = models.TextField(u"Text Field", blank=True)
+#
+#    class Meta:
+#        verbose_name = u'Django Field test'
+#        verbose_name_plural = u'Django Field tests'
+#
+#    def __unicode__(self):
+#        return u'%s' % self.char_test
 
 
 class GrappelliFields(models.Model):
-    fk_test   = models.ForeignKey(User, verbose_name=u"FK Autocomplete", blank=True)
-    char_test = models.CharField(u"Char Field", max_length=255, blank=True)
+    fk_test     = models.ForeignKey(User, verbose_name=u"FK Autocomplete", blank=True)
+    m2m_test    = models.ManyToManyField(User, verbose_name=u"M2M Autocomplete", blank=True, related_name="m2m_user")
+    char_test   = models.CharField(u"Char Field", max_length=255, blank=True)
+    inline_test = models.ForeignKey('InlineTabularTest', verbose_name=u"ForeignKey (inline)", blank=True)
+    date_added  = models.DateField(u"DateField", auto_now_add=True, auto_now=True, blank=True, null=True)
+
+    def __unicode__(self):
+        return u'%s' % self.char_test
 
     class Meta:
         verbose_name = u'Grappelli Field test'
         verbose_name_plural = u'Grappelli Field tests'
-
-    def __unicode__(self):
-        return u'%s' % self.char_test
