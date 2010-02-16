458e647b3e9cb19e36537fc3ba1705e9e7c443fc
Klemens Mantzos <klemens@fetzig.at>
2010-02-16 12:52:18 +0000

    * drop current version

--- custom/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from grappelli.custom.models.navigation import Navigation, NavigationItem
-from grappelli.custom.models.bookmarks import Bookmark, BookmarkItem
-from grappelli.custom.models.help import Help, HelpItem
