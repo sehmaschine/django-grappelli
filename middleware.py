e86fdccd0ca7e5e6b28106bbb608915a76eacd7b
Maxime Haineault <haineault@gmail.com>
2010-03-10 23:59:02 +0000

    fixed remaining obsolete js links with the JavaScript404Patch middleware

--- middleware.py
+++ middleware.py
@@ -8,7 +8,8 @@ class JavaScript404Patch():
     """
     def __init__(self):
         self.strip = ['actions','SelectBox','SelectFilter2','calendar',
-                      'DateTimeShortcuts', 'CollapsibleGroup']
+                      'DateTimeShortcuts', 'CollapsibleGroup', 'RelatedObjectLookups',
+                      'getElementsBySelector', 'js/core']
         self.re    = re.compile('<script.*(%s).js"></script>\n' % '|'.join(self.strip))
         
     def process_response(self, request, response):
