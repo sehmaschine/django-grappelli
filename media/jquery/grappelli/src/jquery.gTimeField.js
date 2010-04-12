109cb25f8503a3fd19e474b4fb015e3cdb509688
Maxime Haineault <haineault@gmail.com>
2010-04-12 02:51:43 +0000

    - added jquery version of tinymce

--- media/jquery/grappelli/src/jquery.gTimeField.js
+++ media/jquery/grappelli/src/jquery.gTimeField.js
@@ -8,27 +8,49 @@
 
 $.widget('ui.gTimeField', {
 
+    options: {
+        autoSelector: 'input.vTimeField',
+        mask: '99:99:99', // set to false to disable
+        buttons: [
+            {label: gettext("Now"), callback: function(e, ui){ 
+                return ui.element.val(new Date().getHourMinuteSecond()); 
+            }},
+            {label: gettext("Midnight"), callback: function(e, ui){ 
+                return ui.element.val('00:00:00'); 
+            }},
+            {label: gettext("6 a.m."), callback: function(e, ui){ 
+                return ui.element.val('06:00:00'); 
+            }},
+            {label: gettext("Noon"), callback: function(e, ui){ 
+                return ui.element.val('12:00:00'); 
+            }}
+        ]
+    },
+
     _init: function() {
         var ui = this;
         ui.dom = {
-            picker: $('<div class="clockbox module"><h2 class="clock-title" /><ul class="timelist" /><p class="clock-cancel"><a href="#" /></p></div>'),
+            picker: $('<div class="clockbox module"><h2 class="clock-title" /><ul class="timelist" /><p class="clock-cancel"><a /></p></div>'),
             button: $('<button class="ui-timepicker-trigger" type="button" />')
         };
-        ui.dom.picker.appendTo('body')
-            .find('h2').text(gettext('Choose a time')).end()
-            .find('a').text(gettext('Cancel')).end()
-            .css({ display:  'none', position: 'absolute'});
-
+        
         ui.dom.button
             .bind('click.grappelli', function(){
                 ui.toggle(this);
             })
             .insertAfter(ui.element);
 
+        ui.dom.picker.insertAfter(ui.dom.button)
+            .find('h2').text(gettext('Choose a time')).end()
+            .find('a').text(gettext('Cancel')).end()
+            .css({position: 'absolute'}).hide();
+
         $.each(ui.options.buttons, function(){
             var button = this;
-            $('<li><a href="#"></a></li>').find('a')
-                .text(button.label).bind('click.grappelli', function(e){
+            $('<li><a /></li>').find('a')
+                .attr('title', button.label)
+                .text(button.label)
+                .bind('click.grappelli', function(e){
                     button.callback.apply(this, [e, ui]);
                     ui.dom.picker.hide();
                     return false;
@@ -58,45 +80,30 @@ $.widget('ui.gTimeField', {
     show: function(at) {
         var pos = $(at).offset();
         var ui = this;
-        $('.clockbox.module:visible').hide();
-        ui.dom.picker.show().css({
-            top: pos.top - ui.dom.picker.height()/2,
-            left: pos.left + 20
-        });
-        $('body').bind('click.gTimeField', function(e){
-            var target = $(e.originalTarget);
-            if (!target.hasClass('.clock-title') && !target.hasClass('ui-timepicker-trigger')) {
-               ui.hide(); 
-            }
-        });
+
+        ui.dom.picker.css({
+            top: pos.top - ui.dom.picker.height() / 2 - 13,
+            left: pos.left - ui.dom.picker.width(),
+            display: 'block'
+        }).show();
+
+        setTimeout(function(){
+            $('body').one('click.gTimeField', function(e){
+                var target = $(e.originalTarget);
+                if (!target.hasClass('.clock-title') && !target.hasClass('ui-timepicker-trigger')) {
+                   ui.hide(); 
+                }
+            });
+        }, 50);
     },
 
     hide: function() {
         var ui = this;
-        if (ui.dom.picker.is(':visible')) {
-            ui.dom.picker.hide();
-            $('body').unbind('click.gTimeField');
-        }
+        ui.dom.picker.hide();
+        $('body').trigger('click');
+        
     }
 
 });
-
-$.ui.gTimeField.defaults = {
-    mask: '99:99:99', // set to false to disable
-    buttons: [
-        {label: gettext("Now"), callback: function(e, ui){ 
-            return ui.element.val(new Date().getHourMinuteSecond()); 
-        }},
-        {label: gettext("Midnight"), callback: function(e, ui){ 
-            return ui.element.val('00:00:00'); 
-        }},
-        {label: gettext("6 a.m."), callback: function(e, ui){ 
-            return ui.element.val('06:00:00'); 
-        }},
-        {label: gettext("Noon"), callback: function(e, ui){ 
-            return ui.element.val('12:00:00'); 
-        }}
-    ]
-};
 
 })(jQuery);
