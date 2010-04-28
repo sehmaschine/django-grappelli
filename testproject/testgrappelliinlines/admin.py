537d62e8645088e5eb851b94154c4176c52bbd24
Maxime Haineault <haineault@gmail.com>
2010-04-28 13:35:32 +0000

    [sandbox] fixed tests for grappelli stacked inlines

--- testproject/testgrappelliinlines/admin.py
+++ testproject/testgrappelliinlines/admin.py
@@ -6,7 +6,10 @@ from django.conf import settings
 
 from testgrappelli.models import *
 from testgrappelliinlines.models import *
-from grappelli.admin import GrappelliTabularInline
+from grappelli.admin import GrappelliTabularInline, GrappelliStackedInline
+
+
+# Tabular inlines ===================================================================================
 
 # Related Fields  -----------------------------------------------------------------------------------
 
@@ -57,7 +60,7 @@ admin.site.register(GrappelliRelatedFieldsTabularTest, GrappelliRelatedFieldsTab
 
 # Enhanced Fields / Tabular
 
-class GrappelliEnhancedFieldsTabularInline(admin.TabularInline):
+class GrappelliEnhancedFieldsTabularInline(GrappelliTabularInline):
     model = GrappelliEnhancedFieldsInline
     classes = ('ui-collapsible-all-opened', )
     allow_add = True
@@ -83,3 +86,84 @@ class GrappelliEnhancedFieldsTabularTestAdmin(admin.ModelAdmin):
         ]
 
 admin.site.register(GrappelliEnhancedFieldsTabularTest, GrappelliEnhancedFieldsTabularTestAdmin)
+
+
+# Stacked inlines ===================================================================================
+
+# Related Fields  -----------------------------------------------------------------------------------
+
+# Related Fields / Stacked
+
+class GrappelliRelatedFieldsStackedInline(GrappelliStackedInline):
+    model = GrappelliRelatedFieldsInline
+    classes = ('ui-collapsible-all-opened', )
+    allow_add = True
+    extra = 1
+    fieldsets = (
+        ('Autocomplete', {
+            'fields': ('fk_test', 'm2m_test')
+        }),
+        ('Related', {
+            'fields': ('gr_m2m', 'gr_test', )
+        }),
+        ('Generic related', {
+            'fields': ('content_type', 'object_id')
+        }),
+    )
+    autocomplete = {
+        'fk_test': {
+            'search_fields': ('name', 'domain',),
+            'input_format':  '{label:s}',           # optional
+            'list_format':   '{id:d} - {label:s}',  # optional
+        }
+    }
+    facelist = {
+        'm2m_test': {
+            'search_fields': ('name', 'domain',),
+            'input_format':  '{label:s}',           # optional
+            'list_format':   '{id:d} - {label:s}',  # optional
+        }
+    }
+
+class GrappelliRelatedFieldsStackedTestAdmin(admin.ModelAdmin):
+    list_display = ('char_test',)
+    inlines = [GrappelliRelatedFieldsStackedInline]
+
+    class Media:
+        verbose_name = u'Related fields test'
+        verbose_name_plural = u'Related fields tests'
+
+admin.site.register(GrappelliRelatedFieldsStackedTest, GrappelliRelatedFieldsStackedTestAdmin)
+
+# Enhanced Fields  -----------------------------------------------------------------------------------
+
+# Enhanced Fields /Stacked 
+
+class GrappelliEnhancedFieldsStackedInline(GrappelliStackedInline):
+    model = GrappelliEnhancedFieldsInline
+    classes = ('ui-collapsible-all-opened', )
+    allow_add = True
+    extra = 1
+    fieldsets = (
+        (None, {
+            'fields': (
+                'slug_test', 'slug_test2', 'mce_test', 
+            )
+        }),
+    )
+
+class GrappelliEnhancedFieldsStackedTestAdmin(admin.ModelAdmin):
+    list_display = ('char_test',)
+    inlines = [GrappelliEnhancedFieldsStackedInline]
+
+    class Media:
+        verbose_name = u'Enhanced fields test'
+        verbose_name_plural = u'Enhanced fields tests'
+        js = [
+            settings.ADMIN_MEDIA_PREFIX + 'jquery/tinymce/jscripts/tiny_mce/tiny_mce.js',
+            settings.ADMIN_MEDIA_PREFIX + 'tinymce_setup/tinymce_setup.js',
+        ]
+
+admin.site.register(GrappelliEnhancedFieldsStackedTest, GrappelliEnhancedFieldsStackedTestAdmin)
+
+
