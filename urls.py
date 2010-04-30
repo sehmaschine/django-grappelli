3a6d70f550f0e952acb9b35197869125024d9933
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2010-04-30 14:48:47 +0000

    updates, including styles, templates and middleware, nothing special

--- urls.py
+++ urls.py
@@ -7,15 +7,6 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     
-    # BOOKMARKS
-    url(r'^bookmark/add/$', 'grappelli.views.bookmarks.add_bookmark', name="grp_bookmark_add"),
-    url(r'^bookmark/remove/$', 'grappelli.views.bookmarks.remove_bookmark', name="grp_bookmark_remove"),
-    url(r'^bookmark/get/$', 'grappelli.views.bookmarks.get_bookmark', name="grp_bookmark_get"),
-    
-    # HELP
-    url(r'^help/(?P<object_id>\d+)/$', 'grappelli.views.help.detail', name="grp_help_detail"),
-    url(r'^help', 'grappelli.views.help.help', name="grp_help"),
-    
     # GENERIC
     url(r'^lookup/obj/$', 'grappelli.views.generic.generic_lookup', name="grp_generic_lookup"),
     # FOREIGNKEY LOOKUP
