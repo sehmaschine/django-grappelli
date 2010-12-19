a1ec31ea0a1a9b2b9617c5d1a8c7918d765b00ee
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2010-12-19 17:05:53 +0000

    temporary update

--- grappelli/context_processors.py
+++ grappelli/context_processors.py
@@ -1,17 +1,22 @@
 # coding: utf-8
 
+# DJANGO IMPORTS
 from django.conf import settings
 
+
 def admin_template_path(request):
+    """
+    Get Template for Grappelli or Grappelli Admin-Tools
+    """
+    
     # default templates for "grappelli standalone"
-    template_path = "admin/_grappelli/"
+    template_path = "admin/grappelli/"
     
     if getattr(settings, 'ADMIN_TOOLS_INDEX_DASHBOARD', False):
         apps = getattr(settings, 'INSTALLED_APPS')
         if apps.count("admin_tools.dashboard"):
-            # seems to be a grappelli+admin_tools setup
-            # so we use the other templates
-            template_path = "admin/_grappelli_admin_tools/"
+            # use grappelli admintools templates
+            template_path = "admin/grappelli_admin_tools/"
     
     return {
         "admin_template_index": "%sindex.html" % template_path,
