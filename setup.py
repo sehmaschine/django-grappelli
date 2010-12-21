04810a7064d6b4f4de8659ada146a7cfcf331fea
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2010-12-21 12:36:16 +0000

    minor updates

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-grappelli',
-    version='2.2',
+    version='1.3',
     description='A jazzy skin for the Django Admin-Interface.',
     author='Patrick Kranzlmueller, Axel Swoboda (vonautomatisch)',
     author_email='werkstaetten@vonautomatisch.at',
     url='http://code.google.com/p/django-grappelli/',
+    download_url='',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
