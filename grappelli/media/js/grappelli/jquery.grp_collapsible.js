0f2594d5df09eed592fcfc3af618a61ee3a6915a
Patrick Kranzlmueller <patrick@vonautomatisch.at>
2010-12-28 14:57:31 +0000

    collapsible update

--- grappelli/media/js/grappelli/jquery.grp_collapsible.js
+++ grappelli/media/js/grappelli/jquery.grp_collapsible.js
@@ -8,7 +8,7 @@
 (function($) {
     $.fn.grp_collapsible = function(options){
         var defaults = {
-            toggle_handler_slctr: ".collapse-handler",
+            toggle_handler_slctr: ".collapse-handler:first",
             closed_css: "closed",
             open_css: "open",
             on_init: function() {},
@@ -27,12 +27,8 @@
         _register_toggle_handler(elem, options);
     };
     var _register_toggle_handler = function(elem, options) {
-        elem.children(options.toggle_handler_slctr).each(function() {
-            $(this).click(function() {
-                elem.toggleClass(options.closed_css)
-                    .toggleClass(options.open_css);
-                options.on_toggle(elem, options);
-            });
+        elem.children(options.toggle_handler_slctr).click(function() {
+            elem.toggleClass(options.closed_css).toggleClass(options.open_css);
         });
     };
 })(django.jQuery);
