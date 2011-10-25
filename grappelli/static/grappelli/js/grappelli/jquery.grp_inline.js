8997ce912d3e638973507c7edde50a7c05a2cf1c
sehmaschine <patrick@vonautomatisch.at>
2011-10-25 11:08:18 +0200

    fixed a bug with adding class dynamic-form
    see issue #44

--- grappelli/static/grappelli/js/grappelli/jquery.grp_inline.js
+++ grappelli/static/grappelli/js/grappelli/jquery.grp_inline.js
@@ -62,8 +62,9 @@
             // callback
             options.onBeforeInit(form);
             // add options.formCssClass to all forms in the inline
+            // except table/theader/add-item
             if (form.attr('id') !== "") {
-                form.not("." + options.emptyCssClass).addClass(options.formCssClass);
+                form.not("." + options.emptyCssClass).not(".table").not(".thead").not(".add-item").addClass(options.formCssClass);
             }
             // add options.predeleteCssClass to forms with the delete checkbox checked
             form.find("li.delete-handler-container input").each(function() {
