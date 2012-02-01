80e8d11e992d7fec286ab9c55ce0e98528485307
Tomasz Maćkowiak <kurazu@kurazu.net>
2012-02-01 22:04:55 +0100

    polish translation mistake corrected

--- grappelli/static/grappelli/tinymce/jscripts/tiny_mce/plugins/grappelli_contextmenu/langs/pl.js
+++ grappelli/static/grappelli/tinymce/jscripts/tiny_mce/plugins/grappelli_contextmenu/langs/pl.js
@@ -1,5 +1,5 @@
 tinyMCE.addI18n("pl.grappelli_contextmenu",{
-grappelli_contextmenu_insertpbefore_desc:"Wstaw paragraf PRZEZ aktualnym elementem",
+grappelli_contextmenu_insertpbefore_desc:"Wstaw paragraf PRZED aktualnym elementem",
 grappelli_contextmenu_insertpafter_desc:"Wstaw paragraf PO aktualnym elemencie",
 grappelli_contextmenu_insertpbeforeroot_desc:"Wstaw paragraf PRZED aktualnym BAZOWYM elementem",
 grappelli_contextmenu_insertpafterroot_desc:"Wstaw paragraf PO aktualnym BAZOWYM elemencie",
@@ -8,7 +8,7 @@ grappelli_contextmenu_deleteroot_desc:"Usuń aktualny BAZOWY element",
 grappelli_contextmenu_moveup_desc:"PRZENIEŚ aktualny ELEMENT WYŻEJ",
 grappelli_contextmenu_moveuproot_desc:"PRZENIEŚ aktualny BAZOWY element WYŻEJ",
 
-P_grappelli_contextmenu_insertpbefore_desc:"Wstaw paragraf PRZEZ aktualnym elementem",
+P_grappelli_contextmenu_insertpbefore_desc:"Wstaw paragraf PRZED aktualnym elementem",
 P_grappelli_contextmenu_insertpafter_desc:"Wstaw paragraf PO aktualnym elemencie",
 P_grappelli_contextmenu_insertpbeforeroot_desc:"Wstaw paragraf PRZED aktualnym BAZOWYM elementem",
 P_grappelli_contextmenu_insertpafterroot_desc:"Wstaw paragraf PO aktualnym BAZOWYM elemencie",
