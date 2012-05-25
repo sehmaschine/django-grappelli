ed7c866ec491545971e034a6f1ae9d2d53c80603
Parsch <parsch.inc@gmail.com>
2012-05-25 19:34:03 +0200

    start to lay out the grp-doc

--- grappelli/urls.py
+++ grappelli/urls.py
@@ -13,11 +13,17 @@ urlpatterns = patterns('',
     url(r'^lookup/autocomplete/$', 'grappelli.views.related.autocomplete_lookup', name="grp_autocomplete_lookup"),
 
     # Grappelli DOM Documentation
-   (r'^grp-dom-documentation/components', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/components/components.html'}),
-   (r'^grp-dom-documentation/layout-grid', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/layout/layout_grid.html'}),
-   (r'^grp-dom-documentation/layout-fluid', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/layout/layout_fluid.html'}),
-   (r'^grp-dom-documentation/layout-mueller', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/layout/layout_mueller.html'}),
-   (r'^grp-dom-documentation/layouts', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/layout/layouts.html'}),
-   (r'^grp-dom-documentation', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/index.html'}),
+    (r'^grp-dom-documentation/components', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/components/components.html'}),
+    (r'^grp-dom-documentation/layout-grid', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/layout/layout_grid.html'}),
+    (r'^grp-dom-documentation/layout-fluid', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/layout/layout_fluid.html'}),
+    (r'^grp-dom-documentation/layout-mueller', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/layout/layout_mueller.html'}),
+    (r'^grp-dom-documentation/layouts', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/layout/layouts.html'}),
+    (r'^grp-dom-documentation', 'django.views.generic.simple.direct_to_template', {'template': 'grp_dom_documentation/index.html'}),
+
+
+    (r'^grp-doc/modules/', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/modules.html'}),
+    (r'^grp-doc/groups/', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/groups.html'}),
+    (r'^grp-doc/context-navigation/', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/context_navigation.html'}),
+    (r'^grp-doc', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/index.html'}),
     
 )
