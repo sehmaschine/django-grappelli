01e64163501f9dc5469cfcb8ff79cae10135db6f
Parsch <parsch.inc@gmail.com>
2012-05-29 13:34:32 +0200

    updated grp-doc

--- grappelli/urls.py
+++ grappelli/urls.py
@@ -11,5 +11,14 @@ urlpatterns = patterns('',
     url(r'^lookup/related/$', 'grappelli.views.related.related_lookup', name="grp_related_lookup"),
     url(r'^lookup/m2m/$', 'grappelli.views.related.m2m_lookup', name="grp_m2m_lookup"),
     url(r'^lookup/autocomplete/$', 'grappelli.views.related.autocomplete_lookup', name="grp_autocomplete_lookup"),
+
+    # GRAPPELLI DOM DOCUMENTATION
+    (r'^admin/grp-doc/submit-rows/', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/submit_rows.html'}),
+    (r'^admin/grp-doc/modules', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/modules.html'}),
+    (r'^admin/grp-doc/groups/', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/groups.html'}),
+    (r'^admin/grp-doc/navigation/', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/navigation.html'}),
+    (r'^admin/grp-doc/context-navigation/', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/context_navigation.html'}),
+    (r'^admin/grp-doc/basic-page-structure/', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/basic_page_structure.html'}),
+    (r'^admin/grp-doc', 'django.views.generic.simple.direct_to_template', {'template': 'grp_doc/index.html'}),
     
 )
