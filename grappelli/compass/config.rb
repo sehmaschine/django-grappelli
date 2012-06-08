05ac0e3799635a35f981aa4518f21253d11e84e1
Parsch <parsch.inc@gmail.com>
2012-06-08 09:14:33 +0200

    removing unnecessary classes from mixins

--- grappelli/compass/config.rb
+++ grappelli/compass/config.rb
@@ -8,7 +8,7 @@ images_dir = "../static/grappelli/images"
 javascripts_dir = "../static/grappelli/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
