d88c7fd64ed8ea03b9277dc5c2856bcd8f9b4002
Patrick Kranzlmueller <sehmaschine@gmail.com>
2012-06-12 16:18:03 +0200

    updated grp-doc urls

--- grappelli/urls.py
+++ grappelli/urls.py
@@ -14,14 +14,14 @@ urlpatterns = patterns('',
     url(r'^lookup/autocomplete/$', 'grappelli.views.related.autocomplete_lookup', name="grp_autocomplete_lookup"),
 
     # GRAPPELLI DOM DOCUMENTATION
-    url(r'^admin/grp-doc/submit-rows/', TemplateView.as_view(template_name='grp_doc/submit_rows.html'), name="grp_doc_submit_rows"),
-    url(r'^admin/grp-doc/modules', TemplateView.as_view(template_name='grp_doc/modules.html'), name="grp_doc_modules"),
-    url(r'^admin/grp-doc/groups/', TemplateView.as_view(template_name='grp_doc/groups.html'), name="grp_doc_groups"),
-    url(r'^admin/grp-doc/navigation/', TemplateView.as_view(template_name='grp_doc/navigation.html'), name="grp_doc_navigation"),
-    url(r'^admin/grp-doc/context-navigation/', TemplateView.as_view(template_name='grp_doc/context_navigation.html'), name="grp_doc_context_navigation"),
-    url(r'^admin/grp-doc/basic-page-structure/', TemplateView.as_view(template_name='grp_doc/basic_page_structure.html'), name="grp_doc_basic_page_structure"),
-    url(r'^admin/grp-doc/tools/', TemplateView.as_view(template_name='grp_doc/tools.html'), name="grp_doc_tools"),
-    url(r'^admin/grp-doc/object-tools/', TemplateView.as_view(template_name='grp_doc/object_tools.html'), name="grp_doc_object_tools"),
-    url(r'^admin/grp-doc', TemplateView.as_view(template_name='grp_doc/index.html'), name="grp_doc"),
+    url(r'^grp-doc/submit-rows/', TemplateView.as_view(template_name='grp_doc/submit_rows.html'), name="grp_doc_submit_rows"),
+    url(r'^grp-doc/modules', TemplateView.as_view(template_name='grp_doc/modules.html'), name="grp_doc_modules"),
+    url(r'^grp-doc/groups/', TemplateView.as_view(template_name='grp_doc/groups.html'), name="grp_doc_groups"),
+    url(r'^grp-doc/navigation/', TemplateView.as_view(template_name='grp_doc/navigation.html'), name="grp_doc_navigation"),
+    url(r'^grp-doc/context-navigation/', TemplateView.as_view(template_name='grp_doc/context_navigation.html'), name="grp_doc_context_navigation"),
+    url(r'^grp-doc/basic-page-structure/', TemplateView.as_view(template_name='grp_doc/basic_page_structure.html'), name="grp_doc_basic_page_structure"),
+    url(r'^grp-doc/tools/', TemplateView.as_view(template_name='grp_doc/tools.html'), name="grp_doc_tools"),
+    url(r'^grp-doc/object-tools/', TemplateView.as_view(template_name='grp_doc/object_tools.html'), name="grp_doc_object_tools"),
+    url(r'^grp-doc', TemplateView.as_view(template_name='grp_doc/index.html'), name="grp_doc"),
 
 )
