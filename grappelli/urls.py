d94e5f8a4b551200baf83c6ce8e54810dbed71ec
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-11-12 18:42:55 +0100

    [views] Use m2m_related for both fk and m2m.

--- grappelli/urls.py
+++ grappelli/urls.py
@@ -9,7 +9,7 @@ from django.conf import settings
 urlpatterns = patterns('',
     
     # FOREIGNKEY & GENERIC LOOKUP
-    url(r'^lookup/related/$', 'grappelli.views.related.related_lookup', name="grp_related_lookup"),
+    url(r'^lookup/related/$', 'grappelli.views.related.m2m_lookup', name="grp_related_lookup"),
     url(r'^lookup/m2m/$', 'grappelli.views.related.m2m_lookup', name="grp_m2m_lookup"),
     url(r'^lookup/autocomplete/$', 'grappelli.views.related.autocomplete_lookup', name="grp_autocomplete_lookup"),
 
