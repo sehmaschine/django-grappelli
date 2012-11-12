8c7d856558e55857936bf24decdcd0ff0de4ef35
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-11-12 19:25:07 +0100

    [views] Builds class-based generic views from the existing views.

--- grappelli/views/related.py
+++ grappelli/views/related.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 from django.db import models
 from django.db.models.query import QuerySet
 from django.views.decorators.cache import never_cache
+from django.views.generic import View
 from django.utils.translation import ugettext as _
 from django.utils.encoding import smart_str
 import django.utils.simplejson as simplejson
@@ -29,79 +30,84 @@ def get_label(f):
     return f.__unicode__()
 
 
-def check_user_permission(request):
-    user = request.user
-    if not (user.is_active and user.is_staff):
-        raise PermissionDenied
-
-
 def ajax_response(data):
     return HttpResponse(simplejson.dumps(data),
                         mimetype='application/javascript')
 
 
-def model_in_GET(GET):
-    return 'app_label' in GET and 'model_name' in GET
+class M2MLookup(View):
+    def check_user_permission(self):
+        user = self.request.user
+        if not (user.is_active and user.is_staff):
+            raise PermissionDenied
 
+    def model_in_GET(self):
+        GET = self.GET
+        return 'app_label' in GET and 'model_name' in GET
 
-def ajax_GET_view(fn):
-    def inner(request):
-        check_user_permission(request)
-        if request.method == 'GET':
-            out = fn(request)
-            if out is not None:
-                return out
-        data = [{"value" :None, "label": ""}]
-        return ajax_response(data)
+    def has_valid_request(self):
+        return 'object_id' in self.GET and self.model_in_GET()
 
-    return inner
+    def get_model(self):
+        GET = self.GET
+        app_label = GET.get('app_label')
+        model_name = GET.get('model_name')
+        self.model = models.get_model(app_label, model_name)
+        return self.model
 
+    def get_queryset(self):
+        return self.model._default_manager.all()
 
-def get_model_from_GET(GET):
-    app_label = GET.get('app_label')
-    model_name = GET.get('model_name')
-    return models.get_model(app_label, model_name)
-
-
-@never_cache
-@ajax_GET_view
-def m2m_lookup(request):
-    GET = request.GET
-    if 'object_id' in GET and model_in_GET(GET):
-        object_ids = GET.get('object_id').split(',')
-        model = get_model_from_GET(GET)
+    def get_data(self):
+        object_ids = self.GET.get('object_id').split(',')
         data = []
         for object_id in (i for i in object_ids if i):
             try:
-                object = model.objects.get(pk=object_id)
+                object = self.get_queryset().get(pk=object_id)
                 label = get_label(object)
-            except model.DoesNotExist:
+            except self.model.DoesNotExist:
                 label = _("?")
             data.append({"value": object_id, "label": label})
-        if data:
-            return ajax_response(data)
+        return data
+
+    @never_cache
+    def get(self, request, *args, **kwargs):
+        self.check_user_permission()
+        self.GET = self.request.GET
+
+        if self.has_valid_request():
+            self.get_model()
+            data = self.get_data()
+            if data:
+                return ajax_response(data)
+
+        data = [{"value": None, "label": ""}]
+        return ajax_response(data)
 
 
-@never_cache
-@ajax_GET_view
-def autocomplete_lookup(request):
-    GET = request.GET
-    if 'term' in GET and model_in_GET(GET):
+class AutocompleteLookup(M2MLookup):
+    def has_valid_request(self):
+        return 'term' in self.GET and self.model_in_GET()
+
+    def get_data(self):
+        GET = self.GET
         term = GET.get("term")
-        model = get_model_from_GET(GET)
+        model = self.model
         filters = {}
         # FILTER
         if GET.get('query_string', None):
             for item in GET.get('query_string').split("&"):
-                if item.split("=")[0] != "t":
-                    filters[smart_str(item.split("=")[0])]=smart_str(item.split("=")[1])
+                k, v = item.split("=")
+                if k != "t":
+                    filters[smart_str(k)] = smart_str(v)
         # SEARCH
-        qs = model._default_manager.filter(**filters)
+        qs = self.get_queryset().filter(**filters)
         for bit in term.split():
-            search = [models.Q(**{smart_str(item):smart_str(bit)}) for item in model.autocomplete_search_fields()]
+            search = [models.Q(**{smart_str(item):smart_str(bit)})
+                                for item in model.autocomplete_search_fields()]
             search_qs = QuerySet(model)
             search_qs.dup_select_related(qs)
             search_qs = search_qs.filter(reduce(operator.or_, search))
             qs &= search_qs
-        data = [{"value": f.pk, "label": get_label(f)} for f in qs[:AUTOCOMPLETE_LIMIT]]
-        return ajax_response(data)
+        return [{"value": f.pk, "label": get_label(f)}
+                                              for f in qs[:AUTOCOMPLETE_LIMIT]]
