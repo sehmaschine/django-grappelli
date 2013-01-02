0e1fb2ec792fed5cb80808cb77608c45bad28d85
Jonas Haag <jonas@lophus.org>
2013-01-02 22:34:45 +0100

    Make setup.py work with Python 3
    codecs.open(foo) == open(foo) for text files

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import os
-import codecs
 from setuptools import setup, find_packages
 
 def read(fname):
-    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='django-grappelli',
