4078fc9b9af229328e56a58dc60d383fc02e98e5
Patrick Kranzlmueller <sehmaschine@gmail.com>
2013-12-19 14:31:17 +0100

    removed custom theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 
 import sys, os
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -92,8 +94,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
-html_theme = 'sphinx_grappelli'
+html_theme = 'default'
+#html_theme = 'sphinx_grappelli'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -102,7 +104,7 @@ html_theme = 'sphinx_grappelli'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = ['.']
-html_theme_path = ["_themes",]
+#html_theme_path = ["_themes",]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -217,3 +219,8 @@ man_pages = [
     ('index', 'djangograppelli', u'Django Grappelli Documentation',
      [u'Patrick Kranzlmueller'], 1)
 ]
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
