9331597b3e239a23f85817000d7f9251c8172057
morlandi <morlandi@brainstorm.it>
2014-02-06 12:39:07 +0100

    Typo fix

--- grappelli/static/grappelli/jquery/i18n/ui.datepicker-it.js
+++ grappelli/static/grappelli/jquery/i18n/ui.datepicker-it.js
@@ -10,7 +10,7 @@
 		'Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
 		monthNamesShort: ['Gen','Feb','Mar','Apr','Mag','Giu',
 		'Lug','Ago','Set','Ott','Nov','Dic'],
-		dayNames: ['Domenica','Lunedì','Martedì','Mercoledì','Giiovedì','Venerdì','Sabato'],
+		dayNames: ['Domenica','Lunedì','Martedì','Mercoledì','Giovedì','Venerdì','Sabato'],
 		dayNamesShort: ['Dom','Lun','Mar','Mer','Gio','Ven','Sab'],
 		dayNamesMin: ['Do','Lu','Ma','Me','Gi','Ve','Sa'],
 		dateFormat: 'dd-mm-yy',
