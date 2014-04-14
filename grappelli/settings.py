e2fc00c193f3f3d5f1f158b760c33a2b5881f86b
Patrick Kranzlmueller <sehmaschine@gmail.com>
2014-04-14 11:32:11 +0200

    change settings/urls (2.6.x)

--- grappelli/settings.py
+++ grappelli/settings.py
@@ -7,10 +7,6 @@ from django.conf import settings
 ADMIN_HEADLINE = getattr(settings, "GRAPPELLI_ADMIN_HEADLINE", 'Grappelli')
 ADMIN_TITLE = getattr(settings, "GRAPPELLI_ADMIN_TITLE", 'Grappelli')
 
-# Link to your Main Admin Site (no slashes at start and end)
-# not needed anymore
-ADMIN_URL = getattr(settings, "GRAPPELLI_ADMIN_URL", '/admin/')
-
 # Autocomplete Limit
 AUTOCOMPLETE_LIMIT = getattr(settings, "GRAPPELLI_AUTOCOMPLETE_LIMIT", 10)
 # Alternative approach to define autocomplete search fields
