6c755a4a7114213bd6dd29118ec028e905d636bd
Patrick Kranzlmueller <sehmaschine@gmail.com>
2014-10-17 13:15:46 +0200

    changed dev version for 2.5.x to 2.4.12

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name='django-grappelli',
-    version='2.4.11',
+    version='2.4.12',
     description='A jazzy skin for the Django Admin-Interface.',
     long_description = read('README.rst'),
     author='Patrick Kranzlmueller, Axel Swoboda (vonautomatisch)',
