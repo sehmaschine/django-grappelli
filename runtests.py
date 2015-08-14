e05067083991460ee50e97c772821b2459d19637
Adam Chainz <adam@adamj.eu>
2015-08-14 15:38:40 +0100

    Add runtests script and tox config

---
+++ runtests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+import sys
+
+import pytest
+
+
+def main():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    sys.path.insert(0, "test_project")
+    return pytest.main()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
