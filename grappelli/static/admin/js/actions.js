9bb2e8e32ff29184c36d506f13e86c4f0dbe8980
Patrick Kranzlmueller <sehmaschine@gmail.com>
2016-09-26 15:13:12 +0200

    fixed actions

--- grappelli/static/admin/js/actions.js
+++ grappelli/static/admin/js/actions.js
@@ -1,47 +1,13 @@
-/**
- * GRAPPELLI ACTIONS.JS
- * minor modifications compared with the original js
- *
- */
-
+/*global gettext, interpolate, ngettext*/
 (function($) {
+    'use strict';
     var lastChecked;
 
     $.fn.actions = function(opts) {
         var options = $.extend({}, $.fn.actions.defaults, opts);
         var actionCheckboxes = $(this);
         var list_editable_changed = false;
-        var checker = function(checked) {
-            if (checked) {
-                showQuestion();
-                $(actionCheckboxes).prop("checked", true)
-                    .parent().parent().addClass(options.selectedClass);
-            } else {
-                reset();
-                $(actionCheckboxes).prop("checked", false)
-                    .parent().parent().removeClass(options.selectedClass);
-            }
-        },
-        updateCounter = function() {
-            var sel = $(actionCheckboxes).filter(":checked").length;
-            $(options.counterContainer).html(interpolate(
-            ngettext('%(sel)s of %(cnt)s selected', '%(sel)s of %(cnt)s selected', sel), {
-                sel: sel,
-                cnt: _actions_icnt
-            }, true));
-            $(options.allToggle).prop("checked", function() {
-                var value;
-                if (sel == actionCheckboxes.length) {
-                    value = true;
-                    showQuestion();
-                } else {
-                    value = false;
-                    clearAcross();
-                }
-                return value;
-            });
-        },
-        showQuestion = function() {
+        var showQuestion = function() {
             $(options.acrossClears).hide();
             $(options.acrossQuestions).show();
             $(options.allContainer).hide();
@@ -52,19 +18,48 @@
             $(options.actionContainer).toggleClass(options.selectedClass);
             $(options.allContainer).show();
             $(options.counterContainer).hide();
-            $(options.counterContainer).parent('li').hide();
         },
         reset = function() {
             $(options.acrossClears).hide();
             $(options.acrossQuestions).hide();
             $(options.allContainer).hide();
             $(options.counterContainer).show();
-            $(options.counterContainer).parent('li').show();
         },
         clearAcross = function() {
             reset();
             $(options.acrossInput).val(0);
             $(options.actionContainer).removeClass(options.selectedClass);
+        },
+        checker = function(checked) {
+            if (checked) {
+                showQuestion();
+            } else {
+                reset();
+            }
+            $(actionCheckboxes).prop("checked", checked)
+                .parent().parent().toggleClass(options.selectedClass, checked);
+        },
+        updateCounter = function() {
+            var sel = $(actionCheckboxes).filter(":checked").length;
+            // data-actions-icnt is defined in the generated HTML
+            // and contains the total amount of objects in the queryset
+            var actions_icnt = $('.action-counter').data('actionsIcnt');
+            $(options.counterContainer).html(interpolate(
+            ngettext('%(sel)s of %(cnt)s selected', '%(sel)s of %(cnt)s selected', sel), {
+                sel: sel,
+                cnt: actions_icnt
+            }, true));
+            $(options.allToggle).prop("checked", function() {
+                var value;
+                if (sel === actionCheckboxes.length) {
+                    value = true;
+                    showQuestion();
+                } else {
+                    value = false;
+                    clearAcross();
+                }
+                return value;
+            });
         };
         // Show counter by default
         $(options.counterContainer).show();
@@ -72,7 +67,7 @@
         $(this).filter(":checked").each(function(i) {
             $(this).parent().parent().toggleClass(options.selectedClass);
             updateCounter();
-            if ($(options.acrossInput).val() == 1) {
+            if ($(options.acrossInput).val() === 1) {
                 showClear();
             }
         });
@@ -80,12 +75,12 @@
             checker($(this).prop("checked"));
             updateCounter();
         });
-        $(options.acrossQuestions + " a").click(function(event) {
+        $("a", options.acrossQuestions).click(function(event) {
             event.preventDefault();
             $(options.acrossInput).val(1);
             showClear();
         });
-        $(options.acrossClears + " a").click(function(event) {
+        $("a", options.acrossClears).click(function(event) {
             event.preventDefault();
             $(options.allToggle).prop("checked", false);
             clearAcross();
@@ -96,12 +91,12 @@
         $(actionCheckboxes).click(function(event) {
             if (!event) { event = window.event; }
             var target = event.target ? event.target : event.srcElement;
-            if (lastChecked && $.data(lastChecked) != $.data(target) && event.shiftKey === true) {
+            if (lastChecked && $.data(lastChecked) !== $.data(target) && event.shiftKey === true) {
                 var inrange = false;
                 $(lastChecked).prop("checked", target.checked)
                     .parent().parent().toggleClass(options.selectedClass, target.checked);
                 $(actionCheckboxes).each(function() {
-                    if ($.data(this) == $.data(lastChecked) || $.data(this) == $.data(target)) {
+                    if ($.data(this) === $.data(lastChecked) || $.data(this) === $.data(target)) {
                         inrange = (inrange) ? false : true;
                     }
                     if (inrange) {
@@ -114,34 +109,29 @@
             lastChecked = target;
             updateCounter();
         });
-        $('form#grp-changelist-form table#result_list tr').find('td:gt(0) :input').change(function() {
-			list_editable_changed = true;
-		});
-		$('form#grp-changelist-form button[name="index"]').click(function(event) {
-			if (list_editable_changed) {
-				return confirm(gettext("You have unsaved changes on individual editable fields. If you run an action, your unsaved changes will be lost."));
-			}
-		});
-		$('form#grp-changelist-form input[name="_save"]').click(function(event) {
-			var action_changed = false;
-			$('select option:selected', options.actionContainer).each(function() {
-				if ($(this).val()) {
-					action_changed = true;
-				}
-			});
-			if (action_changed) {
-				if (list_editable_changed) {
-					return confirm(gettext("You have selected an action, but you haven't saved your changes to individual fields yet. Please click OK to save. You'll need to re-run the action."));
-				} else {
-					return confirm(gettext("You have selected an action, and you haven't made any changes on individual fields. You're probably looking for the Go button rather than the Save button."));
-				}
-			}
-		});
-
-        // GRAPPELLI CUSTOM: submit on select
-        // $(options.actionSelect).attr("autocomplete", "off").change(function(evt){
-        //     $(this).parents("form").submit();
-        // });
+        $('form#changelist-form table#result_list tr').find('td:gt(0) :input').change(function() {
+            list_editable_changed = true;
+        });
+        $('form#changelist-form button[name="index"]').click(function(event) {
+            if (list_editable_changed) {
+                return confirm(gettext("You have unsaved changes on individual editable fields. If you run an action, your unsaved changes will be lost."));
+            }
+        });
+        $('form#changelist-form input[name="_save"]').click(function(event) {
+            var action_changed = false;
+            $('select option:selected', options.actionContainer).each(function() {
+                if ($(this).val()) {
+                    action_changed = true;
+                }
+            });
+            if (action_changed) {
+                if (list_editable_changed) {
+                    return confirm(gettext("You have selected an action, but you haven't saved your changes to individual fields yet. Please click OK to save. You'll need to re-run the action."));
+                } else {
+                    return confirm(gettext("You have selected an action, and you haven't made any changes on individual fields. You're probably looking for the Go button rather than the Save button."));
+                }
+            }
+        });
     };
     /* Setup plugin defaults */
     $.fn.actions.defaults = {
@@ -152,7 +142,13 @@
         acrossQuestions: "div.grp-changelist-actions li.grp-question",
         acrossClears: "div.grp-changelist-actions li.grp-clear-selection",
         allToggle: "#action-toggle",
-        selectedClass: "grp-selected",
-        actionSelect: "div.grp-changelist-actions select"
+        actionSelect: "div.grp-changelist-actions select",
+        selectedClass: "grp-selected"
     };
+    $(document).ready(function() {
+        var $actionsEls = $('tr input.action-select');
+        if ($actionsEls.length > 0) {
+            $actionsEls.actions();
+        }
+    });
 })(grp.jQuery);
