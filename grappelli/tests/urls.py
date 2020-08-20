0d3de64f82322227b7da8b0d35421b61784acefc
Mr. Senko <atodorov@mrsenko.com>
2020-08-21 00:19:55 +0300

    django.conf.urls.url() -> django.urls.re_path()
    - doesn't keep compatibility with older versions b/c grappelli is
      tested only with Django>=3.0
    - resolves RemovedInDjango40Warning

--- grappelli/tests/urls.py
+++ grappelli/tests/urls.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 
 # DJANGO IMPORTS
-from django.conf.urls import include, url
+from django.urls import re_path
+from django.conf.urls import include
 
 # GRAPPELLI IMPORTS
 from grappelli.tests import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^grappelli/', include('grappelli.urls'))
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^grappelli/', include('grappelli.urls'))
 ]
--- grappelli/urls.py
+++ grappelli/urls.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views.related import AutocompleteLookup, M2MLookup, RelatedLookup
 from .views.switch import switch_user
@@ -8,11 +8,11 @@ from .views.switch import switch_user
 urlpatterns = [
 
     # FOREIGNKEY & GENERIC LOOKUP
-    url(r'^lookup/related/$', RelatedLookup.as_view(), name="grp_related_lookup"),
-    url(r'^lookup/m2m/$', M2MLookup.as_view(), name="grp_m2m_lookup"),
-    url(r'^lookup/autocomplete/$', AutocompleteLookup.as_view(), name="grp_autocomplete_lookup"),
+    re_path(r'^lookup/related/$', RelatedLookup.as_view(), name="grp_related_lookup"),
+    re_path(r'^lookup/m2m/$', M2MLookup.as_view(), name="grp_m2m_lookup"),
+    re_path(r'^lookup/autocomplete/$', AutocompleteLookup.as_view(), name="grp_autocomplete_lookup"),
 
     # SWITCH USER
-    url(r'^switch/user/(?P<object_id>\d+)/$', switch_user, name="grp_switch_user"),
+    re_path(r'^switch/user/(?P<object_id>\d+)/$', switch_user, name="grp_switch_user"),
 
 ]
