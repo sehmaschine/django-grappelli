cf3a52f9fd88d8dacdfd6e20f4aa2b2ec0f8d2af
Patrick Kranzlmueller <sehmaschine@gmail.com>
2023-12-20 13:55:16 +0100

    pushed dev version of 2.15.x to 2.15.8

--- grappelli/__init__.py
+++ grappelli/__init__.py
@@ -1,6 +1,6 @@
 import django
 
-VERSION = '2.15.7'
+VERSION = "2.15.8"
 
 if django.VERSION < (3, 2):
-    default_app_config = 'grappelli.apps.GrappelliConfig'
+    default_app_config = "grappelli.apps.GrappelliConfig"
