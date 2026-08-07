d92a7802adf418af49649146183ad31f1a5a60ed
Michał Pasternak <michal.dtz@gmail.com>
2026-08-07 16:05:34 +0200

    sprites: route the spritesheet URL through --grp-spritesheet

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,7 @@ module.exports = function (grunt) {
         destCss: "grappelli/sass/partials/library/_spritesheet.scss",
         imgPath: `../images/spritesheet-${unixTimestamp}.png`,
         padding: 200,
+        cssTemplate: "tools/theme/spritesmith-template.handlebars",
       },
     },
     exec: {
